b5b0ed555806cb24746a7f573bfcf5a22f1f3f9d
Dhruv Panchal <panchaldhruv29@3512754.no-reply.drupal.org>
2019-04-29 19:01:37 +0530

    Code level changes

--- alter_admin_form/src/Controller/MainPageController.php
+++ alter_admin_form/src/Controller/MainPageController.php
@@ -11,16 +11,24 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class MainPageController extends ControllerBase {
   public function mainPage() {
+	$config = \Drupal::service('config.factory')->getEditable('siteapikey.settings');
+	$config_key = $config->get('site_api_key');
     $current_path = \Drupal::service('path.current')->getPath();
     $current_path_explode = explode('/', $current_path);
     $path_nid = $current_path_explode[2];
+    $path_key = $current_path_explode[3];
     $node = \Drupal\node\Entity\Node::load($path_nid);
 	$nid = $node->id();
 	$title = $node->getTitle();
 	$body = $node->get('body')->getValue();
 	$body_value = $body[0]['value'];
-	$json[] = ['id' => $nid, 'title' => $title, 'body' => $body];
-	return new JsonResponse($json);
+	if($path_key == $config_key) {
+		$json[] = ['id' => $nid, 'title' => $title, 'body' => $body];
+		return new JsonResponse($json);
+	}else{
+		Print 'Please insert correct key.';
+		exit;
+	}
   }
 }
 
